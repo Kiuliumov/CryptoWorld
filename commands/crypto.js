f1827a5937efda3b7ea18a61029078bc82fc71df
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2024-01-23 20:59:14 +0200

    Update crypto.js

--- commands/crypto.js
+++ commands/crypto.js
@@ -1,3 +1,4 @@
+//Coingecko has a limit of daily requests
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { EmbedBuilder } = require("discord.js");
 const axios = require("axios");
